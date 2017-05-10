48db02561cd5931ff7e7b836be999fb08f100c70
Hyun Kim <hkim@live.com.au>
2017-05-10 16:52:03 +1000

    refactored and added delete method

--- controllers/ingredientController.js
+++ controllers/ingredientController.js
@@ -10,3 +10,48 @@ exports.getIngredient = (req,res) => {
       });
     });
 }
+
+exports.editIngredient = (req, res) => {
+  Ingredient.findOne({ _id: req.params.id })
+    .then(ingredient => {
+      res.render('edit',  {ingredient: ingredient});
+    })
+}
+
+exports.createIngredient = (req,res) => {
+  const name = req.body.ingredient_name;
+  let ingredient = new Ingredient();
+  ingredient.name = name;
+  ingredient.save()
+    .then(()=>{
+      res.redirect('/')
+    })
+  }
+
+exports.updateIngredient = (req, res) => {
+    // console.log('req.body:', req.body);
+    Ingredient.findOneAndUpdate({ _id: req.params.id }, req.body
+      ,{
+        new: true
+    })
+    .then(ingredient =>{
+      res.redirect('/')
+    })
+}
+
+exports.deleteIngredient = (req, res) => {
+  Ingredient.findOne({ _id: req.params.id })
+    .then(ingredient => {
+      ingredient.remove()
+        .then(()=>{
+          res.redirect('/')
+        })
+    })
+}
+
+exports.sendApi = (req,res) =>{
+  Ingredient.findOne({ _id: req.params.id})
+    .then(ingredient => {
+      res.json(ingredient)
+    })
+}
--- routes/index.js
+++ routes/index.js
@@ -7,35 +7,16 @@ const ingredientController = require('../controllers/ingredientController');
 /* GET home page. */
 // request is what we are sending
 // response is what are receiving
-router.get('/ingredient/:id/edit', (req, res) => {
-  Ingredient.findOne({ _id: req.params.id })
-    .then(ingredient => {
-      res.render('edit',  {ingredient: ingredient});
-    })
-});
+router.get('/ingredient/:id/edit', ingredientController.editIngredient);
 
 router.get('/', ingredientController.getIngredient);
 
-router.post('/', (req,res)=>{
-  const name = req.body.ingredient_name;
-  let ingredient = new Ingredient();
-  ingredient.name = name;
-  ingredient.save()
-    .then(()=>{
-      res.redirect('/')
-    })
-})
+router.post('/', ingredientController.createIngredient);
 
+router.post('/ingredient/:id/edit', ingredientController.updateIngredient);
 
-router.post('/ingredient/:id/edit', (req,res) =>{
-  console.log('req.body:', req.body);
-  Ingredient.findOneAndUpdate({ _id: req.params.id }, req.body
-    ,{
-      new: true
-  })
-  .then(ingredient =>{
-    res.redirect('/')
-  })
-})
+router.get('/destroy/:id', ingredientController.deleteIngredient);
+
+router.get('/api/:id', ingredientController.sendApi)
 
 module.exports = router;
